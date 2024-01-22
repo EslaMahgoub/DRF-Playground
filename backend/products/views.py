b5a64f3647721860b20270be3b725d87de385ebb
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2024-01-22 14:18:05 +0100

    Add Authenticaion and permissions remove unused function based views

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,10 +2,13 @@ from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from api.mixins import StaffEditorPermissionMixin
+
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
+class ProductListCreateAPIView(StaffEditorPermissionMixin, 
+                               generics.ListCreateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
 
@@ -17,20 +20,19 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     if content is None:
       content = title
     serializer.save(content=content)
-    # send a Django Signal
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductDetailAPIView(StaffEditorPermissionMixin, 
+                           generics.RetrieveAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
   # lookup_fields = 'pk'
 
-
 product_detail_view = ProductDetailAPIView.as_view()
 
-
-class ProductUpdateAPIView(generics.UpdateAPIView):
+class ProductUpdateAPIView(StaffEditorPermissionMixin, 
+                           generics.UpdateAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
   lookup_fields = 'pk'
@@ -41,54 +43,16 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
       instance.content = instance.title
       ## serializer.save()
 
-
 product_update_view = ProductUpdateAPIView.as_view()
 
-class ProductDestroyAPIView(generics.DestroyAPIView):
+class ProductDestroyAPIView(StaffEditorPermissionMixin, 
+                            generics.DestroyAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
   lookup_fields = 'pk'
 
   def perform_destory(self, instance):
-      # instance
       super().perform_destroy(instance)
 
 
 product_delete_view = ProductDestroyAPIView.as_view()
-
-# class ProductListAPIView(generics.ListAPIView):
-#   """
-#   Not Gonna use it because we can use ListCreateAPIViewe at the top
-#   """
-#   queryset = Product.objects.all()
-#   serializer_class = ProductSerializer
-#   # lookup_fields = 'pk'
-
-# product_detail_view = ProductDetaiAPIView.as_view()
-
-  #   return self.list(request, *args, **kwargs)
-
-# combine list, detail and create in one method
-# @api_view(['GET', 'POST'])
-# def product_alt_view(request, pk=None, *args, **kwargs):
-#   method = request.method # PUT -> update and DESTROY -> delete
-#   if method == 'GET':
-#     if pk is not None:
-#       obj = get_object_or_404(Product, pk=pk)
-#       data = ProductSerializer(obj, many=False).data
-#       return Response(data)
-#     queryset = Product.objects.all()
-#     data = ProductSerializer(queryset, many=True).data
-#     return Response(data)
-
-#   if method == 'POST':
-#     serializer = ProductSerializer(data=request.data)
-#     if serializer.is_valid(raise_exception=True):
-#       title = serializer.validated_data.get('title')
-#       content = serializer.validated_data.get('content')
-#       if content is None:
-#         content = title
-#       serializer.save(content=content)
-#       print(serializer.data)
-#       return Response(serializer.data)
-#     return Response({"invalid": "Not valid"}, status=400)
