317a32954453439b0465a0b1cfde1c2ffdc091e8
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2024-01-24 03:06:13 +0100

    Add search feature to js client

--- js_client/client.js
+++ js_client/client.js
@@ -1,9 +1,16 @@
 const contentContainer = document.getElementById('content-container')
 const loginForm = document.getElementById('login-form')
+const searchForm = document.getElementById('search-form')
 const baseEndpoint = "http://localhost:8000/api"
+
 if (loginForm) {
   // handle login form
   loginForm.addEventListener('submit', handleLogin)
+}
+
+if (searchForm) {
+  // handle login form
+  searchForm.addEventListener('submit', handleSearch)
 }
 
 function handleLogin(event) {
@@ -31,6 +38,52 @@ function handleLogin(event) {
     console.log('err', err)
   })
 }
+
+function handleSearch(event) {
+  event.preventDefault()
+  let formData = new FormData(searchForm)
+  let data = Object.fromEntries(formData)
+  let searchParams = new URLSearchParams(data)
+  const endpoint = `${baseEndpoint}/search/?${searchParams}`
+  const headers = {
+    "Content-Type": "application/json",
+  }
+  const authToken = localStorage.getItem('access')
+  if (authToken){
+    headers['Authorization'] = `Bearer ${authToken}`
+  }
+  const options = {
+    method: "GET",
+    headers: headers
+  }
+  fetch(endpoint, options)
+  .then(response=>{
+    return response.json()
+  })
+  .then(data => {
+    const validData = isTokenNotValid(data)
+      if (validData && contentContainer){
+          contentContainer.innerHTML = ""
+          if (data && data.hits) {
+              let htmlStr  = ""
+              for (let result of data.hits) {
+                  htmlStr += "<li>"+ result.title + "</li>"
+              }
+              contentContainer.innerHTML = htmlStr
+              if (data.hits.length === 0) {
+                  contentContainer.innerHTML = "<p>No results found</p>"
+              }
+          } else {
+              contentContainer.innerHTML = "<p>No results found</p>"
+          }
+      }
+  })
+  .catch(err=> {
+    console.log('err', err)
+  })
+}
+
+
 
 function handleAuthData(authData, callback) {
   localStorage.setItem('access', authData.access);
@@ -84,7 +137,6 @@ function validateJWTToken() {
   .then(response=>response.json())
   .then(x=> {
       // refresh token
-      isTokenNotValid(x)
   })
 }
 
